77083a3b051d027187c7bc39c9bd71d1a6e01502
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2025-01-10 22:58:49 -0300

    treinando mais exercicios

---
+++ exercicios/desafio2/função.js
@@ -0,0 +1,16 @@
+let num = [1,2,3,4];
+let num2 = [2,4,5,6,7,8];
+
+function verificador(arr){
+    if(arr.length >= 5){
+        console.log('maior valor')
+    }else{
+        console.log('menor valor')
+    }
+}
+verificador(num)
+verificador(num2)
+  
+
+
+   
---
+++ exercicios/desafio7/função.js
@@ -0,0 +1,16 @@
+let num = [1,2,3,4,5];
+function verificador(arr){
+   for(i = 0; i < arr.length ; i++){
+    console.log(`${arr[i]}`)
+   }
+   // vc tambem pode separar eles tipo assim console.log(``${i} tem ${arr[i]}`)
+    
+}
+
+
+verificador(num);
+
+  
+
+
+   
