985ba985e38dc57b8f4e1afb0f6e33dddec60076
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2025-02-01 15:53:07 -0300

    desafios

--- exercicios/ex14/função.js
+++ exercicios/ex14/função.js
@@ -1,5 +1,29 @@
-//callback
+//Escreva uma função que receba um número e retorne um array com todos os números primos até aquele número.
 
+function primosAnetN(num){
+    let primos = [];
+    for(let i = 2 ; i <= num; i++){
+        if(filtragem(i)){
+            primos.push(i);
+        }
+    }
+       return primos;
+}
+
+
+function filtragem(num){
+    for(let i = 2; i < num; i++){
+        if(num % i === 0){
+            return false;
+        }
+    }
+
+    return num > 1;
+}
+
+
+console.log(primosAnetN(10));
+ 
 
 
 
